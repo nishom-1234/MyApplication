9f75673241246534ea364569d63838432ad0ee61
nishom-1234 <mohsin.r@theentertainerasia.com>
2023-10-24 23:30:36 +0500

    Latest code push

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -26,6 +27,8 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        Log.d("createdProject","project")
     }
 }
 
